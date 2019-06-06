8f4e1c4bbf49fb3c356e7f0af06535977eb3cb9b
Armin Kianian <arminkia.dev@gmail.com>
2019-06-06 10:55:59 +0430

    temperature module

---
+++ src/components/temperature/calculator.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import TemperatureInput from "./temperatureInput";
+import BoilingVerdict from "./boilingVerdict";
+import { tryConvert, toCelsius, toFahrenheit } from "./utils";
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { temperature: "", scale: "c" };
+  }
+
+  handleCelsiusChange = temperature => {
+    this.setState({ scale: "c", temperature });
+  };
+
+  handleFahrenheitChange = temperature => {
+    this.setState({ scale: "f", temperature });
+  };
+
+  render() {
+    const { scale, temperature } = this.state;
+
+    const celsius =
+      scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit =
+      scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+
+        <BoilingVerdict celsius={parseFloat(celsius)} />
+      </div>
+    );
+  }
+}
+
+export default Calculator;
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import Game from "./components/game/game";
 import Clock from "./components/dateTime/clock";
 import Toggle from "./components/test/toggle";
-import BasicExample from "./components/formik/basicExample";
+import Calculator from "./components/temperature/calculator";
 
-ReactDOM.render(<BasicExample />, document.getElementById("root"));
+ReactDOM.render(<Calculator />, document.getElementById("root"));
